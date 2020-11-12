829b73a75151710dd9b0386fd2ae32edb551a762
ciamac-da <ciamac.davoudi@gmail.com>
2020-11-12 21:21:36 +0100

    Verified by Cia v1.0.3

--- input.js
+++ input.js
@@ -1,6 +1,6 @@
 // !Teams array
 let groups = [
-    ['Al Ahli FC', 'Esteghlal FC', 'Al Shorta', 'Al Hilal'],
+    ['Al Ahli FC', 'Esteghlal(TAJ) FC', 'Al Shorta', 'Al Hilal'],
     ['Pakhtakor', 'Shabab Dubai', 'Shahr Khodro', 'Tractor Sazi'],
     ['Persepolis', 'Al Taawoun', 'Al Duhail', 'Sharjah'] ,  
     ['Al Nassr', 'Al Saad', 'Sepahan', 'Al Ain'],   
@@ -15,4 +15,40 @@ exports.groupStage = () =>{
         groupList = groups[i]
         console.log(`Group ${i+1}`, groupList);
     }
+}
+
+exports.groupObj = {
+    A1:{
+        name: "Al Ahli FC",
+        points: 0,
+        goalsScored: 0,
+        goalsConceded: 0,
+        goalDifference: 0
+
+    },
+    A2:{
+        name: "Esteghlal(TAJ) FC",
+        points: 0,
+        goalsScored: 0,
+        goalsConceded: 0,
+        goalDifference: 0
+
+    },
+    A3:{
+        name: "AL Shorta",
+        points: 0,
+        goalsScored: 0,
+        goalsConceded: 0,
+        goalDifference: 0
+
+    },
+    A4:{
+        name: "AL Hilal",
+        points: 0,
+        goalsScored: 0,
+        goalsConceded: 0,
+        goalDifference: 0
+
+    },
+    
 }
