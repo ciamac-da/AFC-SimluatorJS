fbf81b7ad2037f25e5c259de13870bdf24ee7905
ciamac-da <ciamac.davoudi@gmail.com>
2020-11-12 21:06:17 +0100

    Verified by Cia v1.0.1

---
+++ input.js
@@ -0,0 +1,11 @@
+// !Teams array
+let groups = [
+    ['Al Ahli FC', 'Esteghlal FC', 'Al Shorta', 'Al Hilal'],
+    ['Pakhtakor', 'Shabab Dubai', 'Shahr Khodro', 'Tractor Sazi'],
+    ['Persepolis', 'Al Taawoun', 'Al Duhail', 'Sharjah'] ,  
+    ['Al Nassr', 'Al Saad', 'Sepahan', 'Al Ain'],   
+    ['FC Seoul', 'Beijing FC', 'Melbourne Victory', 'Chiangrai'],
+    ['Guangzhou', 'Vissel Kobe', 'Shanghai SIPG', 'Sydney FC'],
+    ['Perth Glory', 'Shanghai Shenhua', 'Johor Darul', 'Yokohama F.Marinos'],
+    ['FC Tokyo', 'Suwon Samsung', 'Jeonbuk Hyundai', 'Ulsan Hyundai' ], 
+]
